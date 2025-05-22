32a73c1303165033fc46ee1f892a058295d13fc1
ania221B <100519699+ania221B@users.noreply.github.com>
2025-05-22 23:42:59 +0200

    button changed on blog page

--- src/components/sections/BlogSection.jsx
+++ src/components/sections/BlogSection.jsx
@@ -1,18 +1,32 @@
 import { blogPreviews } from '../../data/blogPreviews'
+import { usePageLocation } from '../../hooks'
 import { Button, SectionHeader } from '../common'
 import BlogArticleList from '../lists/BlogArticleList'
 
 function BlogSection () {
+  const path = usePageLocation()
+  const isBlogPage = path === '/blog'
+
   return (
     <section className='blog section deco deco--diamond'>
       <div className='container flow'>
         <SectionHeader title={`What's new on the blog`}>
-          <Button
-            buttonText={'visit blog'}
-            path={'blog'}
-            isLink={true}
-            isLines={true}
-          ></Button>
+          {isBlogPage ? (
+            <Button
+              buttonText={'see all'}
+              // path={'blog'}
+              isLink={true}
+              isLines={true}
+              ariaLabel={'View all blog posts'}
+            ></Button>
+          ) : (
+            <Button
+              buttonText={'visit blog'}
+              path={'blog'}
+              isLink={true}
+              isLines={true}
+            ></Button>
+          )}
         </SectionHeader>
 
         <BlogArticleList list={blogPreviews}></BlogArticleList>
