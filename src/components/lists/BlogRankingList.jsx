c598d106cec6a8a793e85e345ab4d301744bc642
ania221B <100519699+ania221B@users.noreply.github.com>
2025-05-22 23:30:12 +0200

    popular articles list added

---
+++ src/components/lists/BlogRankingList.jsx
@@ -0,0 +1,20 @@
+import { usePageLocation } from '../../hooks'
+import BlogArticleCard from '../ui/BlogArticleCard'
+
+function BlogRankingList ({ list }) {
+  //   const path = usePageLocation()
+  //   const isBlogPage = path === '/blog'
+
+  return (
+    <ol className='articles grid-auto-fit margin-xl padding-0' role='list'>
+      {list.map(item => {
+        return (
+          <li key={item.id} className='flow'>
+            <BlogArticleCard item={item}></BlogArticleCard>
+          </li>
+        )
+      })}
+    </ol>
+  )
+}
+export default BlogRankingList
