db7b286ab3a837df387f0c5132649788aae2ba65
ania221B <100519699+ania221B@users.noreply.github.com>
2025-05-22 23:29:20 +0200

    blog article list component modified to accommodate for styling variants

--- src/components/lists/BlogArticleList.jsx
+++ src/components/lists/BlogArticleList.jsx
@@ -1,8 +1,19 @@
+import { usePageLocation } from '../../hooks'
 import BlogArticleCard from '../ui/BlogArticleCard'
 
 function BlogArticleList ({ list }) {
+  const path = usePageLocation()
+  const isBlogPage = path === '/blog'
+
   return (
-    <ul className='articles grid-auto-fit margin-xl padding-0' role='list'>
+    <ul
+      className={
+        isBlogPage
+          ? 'articles blog-featured margin-xl padding-0'
+          : 'articles grid-auto-fit margin-xl padding-0'
+      }
+      role='list'
+    >
       {list.map(item => {
         return (
           <li key={item.id} className='flow'>
