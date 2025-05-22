ea0a734c086164db6283054108dbce1e69103b57
ania221B <100519699+ania221B@users.noreply.github.com>
2025-05-22 23:45:41 +0200

    content added to blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,22 @@
-import { UnderConstruction } from '../components/common'
-import { HeroSection } from '../components/sections'
+import {
+  AboutSection,
+  BlogSection,
+  CTASection,
+  HeroSection,
+  PopularPosts,
+  ProductFeatureSection
+} from '../components/sections'
 
 function Blog () {
-  return <UnderConstruction></UnderConstruction>
+  return (
+    <>
+      <HeroSection title='Blog'></HeroSection>
+      <BlogSection></BlogSection>
+      <PopularPosts></PopularPosts>
+      <AboutSection></AboutSection>
+      <ProductFeatureSection></ProductFeatureSection>
+      <CTASection></CTASection>
+    </>
+  )
 }
 export default Blog
