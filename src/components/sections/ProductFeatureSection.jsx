573d069a575d4e1315e2d9a06ab37334a752a344
ania221B <100519699+ania221B@users.noreply.github.com>
2025-05-22 23:52:42 +0200

    variation in decoration added

--- src/components/sections/ProductFeatureSection.jsx
+++ src/components/sections/ProductFeatureSection.jsx
@@ -1,11 +1,22 @@
+import { usePageLocation } from '../../hooks'
 import FeatureList from '../lists/FeatureList'
 
 function ProductFeatureSection () {
+  const path = usePageLocation()
+  const isHomePage = path === '/'
+  const isContactPage = path === '/contact'
+  const isBlogPage = path === '/blog'
   return (
     <section
-      className='product-features section bg-primary-100 deco deco--separator deco--separator-top
-            deco--separator-bottom
-            deco--separator-short'
+      className={
+        isHomePage
+          ? 'product-features section bg-primary-100 deco deco--separator deco--separator-top deco--separator-bottom deco--separator-short'
+          : isContactPage
+          ? 'product-features deco deco--line'
+          : isBlogPage
+          ? 'product-features section deco deco--line'
+          : 'product-features section deco deco--separator deco--separator-top deco--separator-bottom deco--separator-short'
+      }
     >
       <div className='container'>
         <FeatureList></FeatureList>
