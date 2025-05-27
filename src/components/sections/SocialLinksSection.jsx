fcc3bc0ec098e75203d9521c12549e0939a1e71a
ania221B <100519699+ania221B@users.noreply.github.com>
2025-05-27 16:27:16 +0200

    title corrected

--- src/components/sections/SocialLinksSection.jsx
+++ src/components/sections/SocialLinksSection.jsx
@@ -3,7 +3,7 @@ import SocialMediaList from '../lists/SocialMediaList'
 function SocialLinksSection () {
   return (
     <section>
-      <h2 className='section__subtitle'>Follow me</h2>
+      <h2 className='section__subtitle'>Follow us</h2>
       <SocialMediaList></SocialMediaList>
     </section>
   )
