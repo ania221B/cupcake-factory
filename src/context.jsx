26bcb0a95aa6f1031bf1e02b390dbe580e8b9def
ania221B <100519699+ania221B@users.noreply.github.com>
2025-05-30 16:32:07 +0200

    added filtering by search & updated AllProducts page

--- src/context.jsx
+++ src/context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { products } from './data'
 
 const GlobalContext = createContext()
 
@@ -14,9 +14,37 @@ function AppContext ({ children }) {
     src: 'assets/hero-pexels-jill-wellington.jpg'
   }
   const [headerHeight, setHeaderHeight] = useState(0)
+
+  const [allProducts, setAllProducts] = useState(products)
+  const [filteredProducts, setFilteredProducts] = useState(products)
+
+  function filterProducts (text) {
+    const term = text.trim().toLowerCase()
+    const newProducts = allProducts.filter(product => {
+      return (
+        product.name.toLowerCase().includes(term) ||
+        product.description.toLowerCase().includes(term)
+      )
+    })
+
+    setFilteredProducts(newProducts)
+
+    console.log('searchTerm:', term)
+    console.log('new product list:', newProducts)
+  }
   return (
     <GlobalContext.Provider
-      value={{ location, heroImageData, headerHeight, setHeaderHeight }}
+      value={{
+        location,
+        heroImageData,
+        headerHeight,
+        setHeaderHeight,
+        allProducts,
+        setAllProducts,
+        filteredProducts,
+        setFilteredProducts,
+        filterProducts
+      }}
     >
       {children}
     </GlobalContext.Provider>
--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,9 +1,11 @@
-import { UnderConstruction } from '../components/common'
+import { FilterMenu, SearchBar } from '../components/common'
 import { ProductList } from '../components/lists'
 import { HeroSection } from '../components/sections'
+import { useGlobalContext } from '../context'
 import { products } from '../data'
 
 function AllProducts () {
+  const { filteredProducts } = useGlobalContext()
   return (
     <>
       <HeroSection title='All Products Go Here'></HeroSection>
@@ -23,187 +25,12 @@ function AllProducts () {
                 nisi. Nam eget dui. Etiam rhoncus.
               </p>
             </header> */}
-            <aside className='products__filter-menu'>
-              <header>
-                <h3>Filter products</h3>
-              </header>
-              <form
-                id='product-filters'
-                className='product-filters js-product-filters'
-              >
-                <fieldset data-filter='roast' className='flow'>
-                  <legend>Type</legend>
-                  <div className='product-filters__control-wrapper'>
-                    <input type='checkbox' name='cakes' id='cakes' />
-                    <label htmlFor='cakes'>Cakes</label>
-                  </div>
-                  <div className='product-filters__control-wrapper'>
-                    <input type='checkbox' name='cookies' id='cookies' />
-                    <label htmlFor='cookies'>Cookies</label>
-                  </div>
-                  <div className='product-filters__control-wrapper'>
-                    <input type='checkbox' name='pastries' id='pastries' />
-                    <label htmlFor='pastries'>Pastries</label>
-                  </div>
-                  <div className='product-filters__control-wrapper'>
-                    <input type='checkbox' name='desserts' id='desserts' />
-                    <label htmlFor='desserts'>Desserts</label>
-                  </div>
-                </fieldset>
 
-                <fieldset data-filter='type' className='flow'>
-                  <legend>Tag</legend>
-                  <div className='product-filters__control-wrapper'>
-                    <input type='checkbox' name='bestseller' id='bestseller' />
-                    <label htmlFor='bestseller'>Bestseller</label>
-                  </div>
-                  <div className='product-filters__control-wrapper'>
-                    <input
-                      type='checkbox'
-                      name='new-arrival'
-                      id='new-arrival'
-                    />
-                    <label htmlFor='new-arrival'>New Arrival</label>
-                  </div>
-                  <div className='product-filters__control-wrapper'>
-                    <input type='checkbox' name='sale' id='sale' />
-                    <label htmlFor='sale'>Sale</label>
-                  </div>
-                </fieldset>
+            <FilterMenu></FilterMenu>
+            <SearchBar></SearchBar>
 
-                <fieldset data-filter='availability' className='flow'>
-                  <legend>Availability</legend>
-                  <div className='product-filters__control-wrapper'>
-                    <input type='checkbox' name='availability' id='in-stock' />
-                    <label htmlFor='availability'>In stock</label>
-                  </div>
-                </fieldset>
-                <fieldset data-filter='price' className='flow'>
-                  <legend>Price</legend>
-                  <div className='price-inputs'>
-                    <div className='product-filters__control-wrapper'>
-                      <input
-                        type='number'
-                        name='price-min'
-                        id='price-min'
-                        min='0'
-                        max='20'
-                        defaultValue='0'
-                        className='js-price-min'
-                      />
-                      <label htmlFor='price-min'>Min price</label>
-                    </div>
-                    <div className='product-filters__control-wrapper'>
-                      <input
-                        type='number'
-                        name='price-max'
-                        id='price-max'
-                        min='0'
-                        max='20'
-                        defaultValue='20'
-                        className='js-price-max'
-                      />
-                      <label htmlFor='price-max'>Max price</label>
-                    </div>
-                  </div>
-                </fieldset>
-              </form>
-              <button
-                type='button'
-                aria-label='Resets all filtering applied'
-                className='btn btn--card reset-button js-reset-btn'
-                data-button='primary'
-              >
-                Reset All
-              </button>
-            </aside>
-            <search className='search-menu'>
-              <form className='search-form js-search-bar'>
-                <div className='form__wrapper'>
-                  <label htmlFor='search'>Search for products:</label>
-                  <input
-                    id='search'
-                    name='search'
-                    type='search'
-                    className='js-search'
-                  />
-                  <button
-                    type='submit'
-                    className='btn btn--card'
-                    data-button='primary'
-                  >
-                    Search
-                  </button>
-                </div>
-              </form>
-            </search>
             <div className='item-area'>
-              {/* <div className='spinner-wrapper js-spinner-wrapper'>
-                <button type='button' className='button spinner js-spinner'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='48'
-                    height='48'
-                    viewBox='0 0 24 24'
-                  >
-                    <g>
-                      <circle
-                        cx='12'
-                        cy='2.5'
-                        r='1.5'
-                        fill='currentColor'
-                        opacity='.14'
-                      />
-                      <circle
-                        cx='16.75'
-                        cy='3.77'
-                        r='1.5'
-                        fill='currentColor'
-                        opacity='.29'
-                      />
-                      <circle
-                        cx='20.23'
-                        cy='7.25'
-                        r='1.5'
-                        fill='currentColor'
-                        opacity='.43'
-                      />
-                      <circle
-                        cx='21.5'
-                        cy='12'
-                        r='1.5'
-                        fill='currentColor'
-                        opacity='.57'
-                      />
-                      <circle
-                        cx='20.23'
-                        cy='16.75'
-                        r='1.5'
-                        fill='currentColor'
-                        opacity='.71'
-                      />
-                      <circle
-                        cx='16.75'
-                        cy='20.23'
-                        r='1.5'
-                        fill='currentColor'
-                        opacity='.86'
-                      />
-                      <circle cx='12' cy='21.5' r='1.5' fill='currentColor' />
-                      <animateTransform
-                        attributeName='transform'
-                        calcMode='discrete'
-                        dur='0.75s'
-                        repeatCount='indefinite'
-                        type='rotate'
-                        values='0 12 12;30 12 12;60 12 12;90 12 12;120 12 12;150 12 12;180 12 12;210 12 12;240 12 12;270 12 12;300 12 12;330 12 12;360 12 12'
-                      />
-                    </g>
-                  </svg>
-                </button>
-              </div> */}
-
-              <ProductList list={products}></ProductList>
+              <ProductList list={filteredProducts}></ProductList>
             </div>
           </div>
         </div>
