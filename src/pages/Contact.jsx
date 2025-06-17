2311de8f80c2b89582f3d07ed3f9e58a39f269b1
ania221B <100519699+ania221B@users.noreply.github.com>
2025-06-17 16:40:16 +0200

    contact details section + its styling added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,7 @@
 import { Button, SectionHeader } from '../components/common'
-import {
-  CTASection,
-  HeroSection,
-  ProductFeatureSection
-} from '../components/sections'
+import { HeroSection, ProductFeatureSection } from '../components/sections'
 import ContactSection from '../components/sections/ContactSection'
+import { footerLinks } from '../data'
 
 function Contact () {
   function handleOrder () {
@@ -14,6 +11,35 @@ function Contact () {
     <>
       <HeroSection title={'Contact us'}></HeroSection>
       <ContactSection></ContactSection>
+      <section className='section'>
+        <div className='container'>
+          {footerLinks
+            .filter(link => link.title === 'contact')
+            .map(link => {
+              return (
+                <ul className='contact-details grid-auto-fit' role='list'>
+                  {link.details.map(detail => {
+                    const { id, icon, text, path, subtext } = detail
+                    return (
+                      <li
+                        key={id}
+                        className='contact-details__item deco deco--separator deco--separator-top'
+                      >
+                        <div className='contact-details__icon'>{icon}</div>
+                        <div>
+                          <h3 className='contact-details__heading'>{text}</h3>
+                          <a href={path} className='force-link-wrap'>
+                            {subtext}
+                          </a>
+                        </div>
+                      </li>
+                    )
+                  })}
+                </ul>
+              )
+            })}
+        </div>
+      </section>
       <section className='section'>
         <div className='container'>
           <div className='order-wrapper text-center'>
@@ -39,8 +65,6 @@ function Contact () {
 
             <ProductFeatureSection></ProductFeatureSection>
           </div>
-
-          <CTASection></CTASection>
         </div>
       </section>
     </>
