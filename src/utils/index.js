9e59bb9214ba0a9a2a380fa8c3cf101d950b8cae
ania221B <100519699+ania221B@users.noreply.github.com>
2025-06-17 16:12:38 +0200

    date formatting utilities added

--- src/utils/index.js
+++ src/utils/index.js
@@ -63,7 +63,7 @@ export function makeHyphenatedLowerCase (string) {
  * @returns formatted price
  */
 export function formatPrice (priceToFormat) {
-  const price = priceToFormat.toString()
+  const price = priceToFormat.toString() || ''
 
   if (price.includes('.')) {
     if (price.length === 5) {
@@ -112,4 +112,44 @@ export function compareMinMax (minVal, maxVal) {
   if (min === null || max === null) return null
   if (max <= min) return 'Max price must be greater than min price'
   return null
+}
+
+/**
+ * Formats date, so that it is displayed in the 3-character month name 1 or 2-digit day, 4-digit year format
+ * @param {Date} date Current date
+ * @returns {String} String with formatted date
+ */
+export function getFormatedDate (dateToFormat) {
+  const date = new Date(dateToFormat)
+  const monthsInAYear = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
+  const year = date.getFullYear()
+  const month = monthsInAYear[date.getMonth()]
+  const day = date.getDate()
+  return `${month} ${day}, ${year}`
+}
+
+/**
+ * Converts date to a hyphenated string for the use in `dateTime` parameter
+ * @param {Date} date Current date
+ * @returns {String} Hyphenated string with date
+ */
+export function getDateTimeString (sourceDate) {
+  const date = new Date(sourceDate)
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate()
+  return `${year}-${month}-${day}`
 }
