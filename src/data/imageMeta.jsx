42bbba5a05710a1d0658a73e90509e7324367022
ania221B <100519699+ania221B@users.noreply.github.com>
2025-06-17 17:16:42 +0200

    file for image metadata added for future use

---
+++ src/data/imageMeta.jsx
@@ -0,0 +1,13 @@
+const imageMeta = {
+  macarons: [
+    {
+      file: 'img1.jpg',
+      alt: 'Stack of colorful macarons on plate surrounded by flowers',
+      title: 'Photo by Diana Light',
+      photographer: {
+        name: 'Diana Light',
+        url: 'https://www.pexels.com/@dreamcatchlight/'
+      }
+    }
+  ]
+}
