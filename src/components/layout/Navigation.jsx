4d38fcaca75179bf8baaabbd397a96385943bb66
ania221B <100519699+ania221B@users.noreply.github.com>
2025-06-17 17:09:58 +0200

    search bar hidden for time being

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -2,8 +2,8 @@ import { useEffect, useRef, useState } from 'react'
 
 import { NavLink } from 'react-router-dom'
 import { RiCloseLargeFill, RiMenuFill } from 'react-icons/ri'
-import { GrFormClose } from 'react-icons/gr'
-import { FaSearch } from 'react-icons/fa'
+// import { GrFormClose } from 'react-icons/gr'
+// import { FaSearch } from 'react-icons/fa'
 import { navLinks } from '../../data/navLinks'
 
 function Navigation () {
@@ -81,7 +81,7 @@ function Navigation () {
         </ul>
 
         <div className='page-nav__extras'>
-          <search className='search-bar'>
+          {/* <search className='search-bar'>
             <input
               type='search'
               placeholder='serach&hellip;'
@@ -94,7 +94,7 @@ function Navigation () {
             >
               <FaSearch />
             </button>
-          </search>
+          </search> */}
 
           {/* make it go to login screen later? */}
           <a href='#' className='btn btn--login btn--lines bg-animation'>
