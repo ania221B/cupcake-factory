8066ad5ce7222f83bb2dfe87edea2f6a7a008864
ania221B <100519699+ania221B@users.noreply.github.com>
2025-07-04 20:19:11 +0200

    formspree provider added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 import AppContext from './context.jsx'
+import { FormspreeProvider } from '@formspree/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContext>
-      <App />
+      <FormspreeProvider project='2767343151327739745'>
+        <App />
+      </FormspreeProvider>
     </AppContext>
   </React.StrictMode>
 )
