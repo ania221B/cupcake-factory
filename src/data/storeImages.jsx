db58a12159700aa7988d3975c00e8e79680f33bd
ania221B <100519699+ania221B@users.noreply.github.com>
2025-07-23 21:57:15 +0200

    import & add store hero images

---
+++ src/data/storeImages.jsx
@@ -0,0 +1,78 @@
+import image1Fallback from '../assets/images/store/img1.jpg'
+import image1MobileAvif from '../assets/images/store/img1-400.avif'
+import image1MobileWebp from '../assets/images/store/img1-400.webp'
+import image1TabletAvif from '../assets/images/store/img1-768.avif'
+import image1TabletWebp from '../assets/images/store/img1-768.webp'
+import image1DesktopAvif from '../assets/images/store/img1-1200.avif'
+import image1DesktopWebp from '../assets/images/store/img1-1200.webp'
+import image2Fallback from '../assets/images/store/img2.jpg'
+import image2MobileAvif from '../assets/images/store/img2-400.avif'
+import image2MobileWebp from '../assets/images/store/img2-400.webp'
+import image2TabletAvif from '../assets/images/store/img2-768.avif'
+import image2TabletWebp from '../assets/images/store/img2-768.webp'
+import image2DesktopAvif from '../assets/images/store/img2-1200.avif'
+import image2DesktopWebp from '../assets/images/store/img2-1200.webp'
+import image3Fallback from '../assets/images/store/img3.jpg'
+import image3MobileAvif from '../assets/images/store/img3-400.avif'
+import image3MobileWebp from '../assets/images/store/img3-400.webp'
+import image3TabletAvif from '../assets/images/store/img3-768.avif'
+import image3TabletWebp from '../assets/images/store/img3-768.webp'
+import image3DesktopAvif from '../assets/images/store/img3-1200.avif'
+import image3DesktopWebp from '../assets/images/store/img3-1200.webp'
+
+export const storeImages = [
+  {
+    id: 'image-1',
+    alt: 'Pink Cake with Flowers',
+    title: 'Photo by Gabriela Medeiros Chaves: Pink Cake with Flowers',
+    fallback: image1Fallback,
+    mobile: {
+      avif: image1MobileAvif,
+      webp: image1MobileWebp
+    },
+    tablet: {
+      avif: image1TabletAvif,
+      webp: image1TabletWebp
+    },
+    desktop: {
+      avif: image1DesktopAvif,
+      webp: image1DesktopWebp
+    }
+  },
+  {
+    id: 'image-2',
+    alt: 'Different flavoured macarons',
+    title: 'Photo by Diana Light: Different flavoured macarons',
+    fallback: image2Fallback,
+    mobile: {
+      avif: image2MobileAvif,
+      webp: image2MobileWebp
+    },
+    tablet: {
+      avif: image2TabletAvif,
+      webp: image2TabletWebp
+    },
+    desktop: {
+      avif: image2DesktopAvif,
+      webp: image2DesktopWebp
+    }
+  },
+  {
+    id: 'image-3',
+    alt: 'Chocolate Cake With Pink Rose On Top',
+    title: 'Photo by Lina Kivaka: Chocolate Cake With Pink Rose On Top',
+    fallback: image3Fallback,
+    mobile: {
+      avif: image3MobileAvif,
+      webp: image3MobileWebp
+    },
+    tablet: {
+      avif: image3TabletAvif,
+      webp: image3TabletWebp
+    },
+    desktop: {
+      avif: image3DesktopAvif,
+      webp: image3DesktopWebp
+    }
+  }
+]
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -5,11 +5,64 @@ import {
   TestimonialSection
 } from '../components/sections'
 import NewArrivalsSection from '../components/sections/NewArrivalsSection'
+import { useGlobalContext } from '../context'
+import { storeImages } from '../data'
 
 function Store () {
+  const { headerHeight } = useGlobalContext()
   return (
     <>
-      <UnderConstruction></UnderConstruction>
+      <section
+        className='section hero--store bg-primary-100 flow'
+        style={{ '--header-height': `${headerHeight}px` }}
+      >
+        <header className='hero__header deco deco--line deco--line text-center'>
+          <h1 className='page-title'>Welcome to our Store</h1>
+        </header>
+
+        <div
+          className='container hero--store__wrapper'
+          data-container='full-bleed'
+        >
+          {storeImages.map(image => {
+            return (
+              <picture className='hero--store__img' key={image.id}>
+                <source
+                  srcSet={image.desktop.avif}
+                  type='image/avif'
+                  media='(min-width: 912px)'
+                />
+                <source
+                  srcSet={image.desktop.webp}
+                  type='image/webp'
+                  media='(min-width: 912px)'
+                />
+                <source
+                  srcSet={image.tablet.avif}
+                  type='image/avif'
+                  media='(min-width: 768px)'
+                />
+                <source
+                  srcSet={image.tablet.webp}
+                  type='image/webp'
+                  media='(min-width: 768px)'
+                />
+                <source
+                  srcSet={image.mobile.webp}
+                  type='image/avif'
+                  media='(max-width: 767px)'
+                />
+                <source
+                  srcSet={image.mobile.webp}
+                  type='image/webp'
+                  media='(max-width: 767px)'
+                />
+                <img src={image.fallback} alt={image.alt} title={image.title} />
+              </picture>
+            )
+          })}
+        </div>
+      </section>
       <NewArrivalsSection></NewArrivalsSection>
       <BestsellersSection></BestsellersSection>
       <OrderPromoSection></OrderPromoSection>
