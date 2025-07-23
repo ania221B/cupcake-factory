817b772a99f64a498bb449cca54ba0603c496bf8
ania221B <100519699+ania221B@users.noreply.github.com>
2025-07-23 21:58:47 +0200

    improve spacing for pages with breadcrumb

--- src/components/layout/BaseLayout.jsx
+++ src/components/layout/BaseLayout.jsx
@@ -6,8 +6,21 @@ import ScrollToTop from './ScrollToTop'
 
 function BaseLayout () {
   const location = usePageLocation()
-  const page = location?.split('/')[1] || 'home'
   const isBookATable = location === '/book-a-table'
+  let page
+
+  if (/^\/store\/[^/]+$/.test(location)) {
+    page = 'product'
+  } else if (/^\/blog\/[^/]+$/.test(location)) {
+    page = 'blog-post'
+  } else if (location.startsWith('/store')) {
+    page = 'store'
+  } else if (location.startsWith('/blog')) {
+    page = 'blog'
+  } else {
+    page = location?.split('/')[1] || 'home'
+  }
+
   return (
     <>
       <ScrollToTop></ScrollToTop>
