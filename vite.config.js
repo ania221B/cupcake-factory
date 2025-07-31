c4641d529ef6e57ece220096c9a27ec127ac544c
ania221B <100519699+ania221B@users.noreply.github.com>
2025-07-31 15:27:53 +0200

    fix issue with .active not being applied properly on GH Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: '/cupcake-factory/',
-  plugins: [react()]
+  build: {
+    rollupOptions: {
+      input: 'indec.html'
+    }
+  }
 })
