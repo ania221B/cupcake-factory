a147598c3a76f8ed93dae0c709dbfa4f0e759425
Ania Buwaj <100519699+ania221B@users.noreply.github.com>
2026-02-13 15:19:02 +0100

    fix property name

--- src/components/layout/SkipToMain.jsx
+++ src/components/layout/SkipToMain.jsx
@@ -2,7 +2,7 @@ function SkipToMain () {
   return (
     <a
       href='#main-content'
-      class='skip-to-main-content bg-primary-500 clr-neutral-100 uppercase'
+      className='skip-to-main-content bg-primary-500 clr-neutral-100 uppercase'
     >
       Skip to main content
     </a>
